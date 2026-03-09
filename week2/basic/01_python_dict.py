8b3936d299960e7d516a9944d8e575db44e672be
Sein0104 <kimsein0104@gmail.com>
2026-03-09 20:42:38 +0900

    깃허브단단히꼬여서수동저장

--- week2/basic/01_python_dict.py
+++ week2/basic/01_python_dict.py
@@ -42,13 +42,16 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    pass
+    test_score = [s["score"] for s in students]
     
     # TODO: 평균 점수를 계산하세요
-    pass
+    average = sum(test_score) / len(test_score)
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    pass
+     above_average_students = []
+    for student in students:
+        if student["score"] >= average:
+            above_average_students.append(student["name"]
     
     return average, above_average_students
 
