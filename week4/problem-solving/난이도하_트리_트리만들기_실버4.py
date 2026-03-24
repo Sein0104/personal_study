bed52e1db26db88eb1b88129798a0214a68a0678
Sein0104 <kimsein0104@gmail.com>
2026-03-24 17:58:57 +0900

    전체 폴더 구조 세팅 및 정답지 백업

---
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -0,0 +1,44 @@
+# 트리 - 트리 만들기 (백준 실버4)
+# 문제 링크: https://www.acmicpc.net/problem/14244
+N, M = map(int, input().split())
+
+for i in range (N - M):
+  print(i, i + 1)
+for j in range (N - M, N - 1):
+  print(N - M, j + 1)
+
+"""
+처음 했던 방법
+node 리스트 안에 리스트로 받는 방식이라
+출력이 리스트로 됨 . .
+N, M = map(int, input().split())
+node = [[0, 0] for _ in range (N - 1)]
+for i in range (N - M):
+  node[i][0] = i
+  node[i][1] = i + 1
+for j in range (N - M, N - 1):
+  node[j][0] = N - M
+  node[j][1] = j + 1
+print(*node, sep="\n")
+"""
+
+
+
+"""
+N, M = map(int, input().split())
+
+parent = 0
+for child in range(1, N):
+    print(parent, child)
+    if parent < N - M:
+        parent += 1
+"""
+
+
+
+"""
+N, M = map(int, input().split())
+
+for i in range(N - 1):
+    print(min(i, N - M), i + 1)
+"""
